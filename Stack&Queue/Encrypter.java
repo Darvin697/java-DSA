4db93ed4b90dcdf7c2618aa45a5159eb5875e0e7
Darvin Johny <johnydarvin@gmail.com>
2024-10-10 21:56:47 +0530

    string

--- Stack&Queue/Encrypter.java
+++ Stack&Queue/Encrypter.java
@@ -1,3 +1,23 @@
 public class Encrypter {
-    
+    public static String change (String str, int key){
+        int newKey = key  % 26;
+        char[] charArray = new char[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            int letterPosition = str.charAt(i) + newKey;
+            if (letterPosition <= 122 ) {
+                charArray[i] = (char) letterPosition;
+                
+            }else {
+                charArray[i] = (char) (96+letterPosition%122);
+
+            }
+        }
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        String value = "zap";
+        System.out.println(change(value, 30));
+    }
 }
